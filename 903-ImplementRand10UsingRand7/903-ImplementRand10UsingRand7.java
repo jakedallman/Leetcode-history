0e735f95376645fb7d49de39853895ef1c3073ab
jakedallman <dallmanjake399@gmail.com>
2025-10-04 14:37:49 -0400

    Create 903-ImplementRand10UsingRand7

---
+++ 903-ImplementRand10UsingRand7/903-ImplementRand10UsingRand7.java
@@ -0,0 +1,16 @@
+// Last updated: 10/4/2025, 2:35:00 PM
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() {
+        int sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            sum+=rand7();
+        }
+        return (sum%10)+1;
+    }
+}
