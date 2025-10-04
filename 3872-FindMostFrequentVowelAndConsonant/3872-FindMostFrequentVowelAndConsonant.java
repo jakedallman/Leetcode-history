52e133dcf55f72f7896af3ff5302a58d02a03243
jakedallman <dallmanjake399@gmail.com>
2025-10-04 14:37:42 -0400

    Create 3872-FindMostFrequentVowelAndConsonant

---
+++ 3872-FindMostFrequentVowelAndConsonant/3872-FindMostFrequentVowelAndConsonant.java
@@ -0,0 +1,27 @@
+// Last updated: 10/4/2025, 2:34:53 PM
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] letters = new int[26];
+        for (int i = 0; i < s.length();i++)
+        {
+            letters[s.charAt(i)-97]++;
+        }
+        int vowel = Math.max(letters[0],letters[4]);
+        vowel = Math.max(vowel, letters[8]);
+        vowel = Math.max(vowel, letters[14]);
+        vowel = Math.max(vowel, letters[20]);
+
+        int consonant = 0;
+        for (int i = 0; i < 26;i++)
+        {
+            if (i != 0 && i != 4 && i!=8 && i!= 14&& i!=20)
+            {
+                if (letters[i] > consonant)
+                {
+                    consonant = letters[i];
+                }
+            }
+        }
+        return consonant+vowel;
+    }
+}
